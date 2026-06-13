d02fe826b56e8db91f23aedee30523af433375a8
Angeluz <angeluznazer1@gmail.com>
2026-06-13 11:29:42 -0300

    adicionando o projeto inicial

---
+++ src/main/java/com/Loja/xBrain/XBrainApplication.java
@@ -0,0 +1,13 @@
+package com.Loja.xBrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XBrainApplication.class, args);
+	}
+
+}
