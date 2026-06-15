16ffa846bee396e35cffce92d1329b2302eaaeef
Angeluz <angeluznazer1@gmail.com>
2026-06-15 18:27:54 -0300

    criei atributos,Construtores e getter and setter para a pasta Relatorios e desenvolvi a lógica em Service para a conseguir criar os relatórios

--- src/main/java/com/Loja/xBrain/dto/Relatorio.java
+++ src/main/java/com/Loja/xBrain/dto/Relatorio.java
@@ -1,4 +1,40 @@
 package com.Loja.xBrain.dto;
 
 public class Relatorio {
+    private String nome;
+    private double valorTotal;
+    private double mediaVendasDiarias;
+
+    public Relatorio() {
+    }
+
+    public Relatorio(String nome, double valorTotal, double mediaVendasDiarias) {
+        this.nome = nome;
+        this.valorTotal = valorTotal;
+        this.mediaVendasDiarias = mediaVendasDiarias;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public double getMediaVendasDiarias() {
+        return mediaVendasDiarias;
+    }
+
+    public void setMediaVendasDiarias(double mediaVendasDiarias) {
+        this.mediaVendasDiarias = mediaVendasDiarias;
+    }
 }
--- src/main/java/com/Loja/xBrain/service/VendaService.java
+++ src/main/java/com/Loja/xBrain/service/VendaService.java
@@ -1,11 +1,14 @@
 package com.Loja.xBrain.service;
 
+import com.Loja.xBrain.dto.Relatorio;
 import com.Loja.xBrain.model.VendaModel;
 import com.Loja.xBrain.repository.VendaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class VendaService {
@@ -23,8 +26,41 @@ public class VendaService {
         if (venda.getDataVenda() == null){
             venda.setDataVenda(LocalDate.now());
         }
-
         return vendaRepository.save(venda);
+    }
+    public List<Relatorio> gerarRelatorio(LocalDate inicio, LocalDate fim){
+        List<VendaModel> todasVendas = vendaRepository.findAll();
+        List<Relatorio> resultado = new ArrayList<>();
+
+        long totalDias = java.time.temporal.ChronoUnit.DAYS.between(inicio,fim) +1;
+        if (totalDias<=0) totalDias = 1;
+
+        for (VendaModel venda : todasVendas) {
+            LocalDate dataVenda = venda.getDataVenda();
+
+            if (!dataVenda.isBefore(inicio) && dataVenda.isAfter(fim)){
+                String nomeVendedor = venda.getNomeVendedor();
+                double valorVenda = venda.getValorVenda();
+                boolean vendedorExiteNaLista= false;
+
+                for (Relatorio r:  resultado) {
+                    if (r.getNome().equals(nomeVendedor)){
+                        r.setValorTotal(r.getValorTotal()+ valorVenda);
+                        vendedorExiteNaLista = true;
+                        break;
+                    }
+                }
+                if (!vendedorExiteNaLista){
+                    Relatorio novoRelatorio = new Relatorio(nomeVendedor, valorVenda, 0.0);
+                    resultado.add(novoRelatorio);
+                }
+            }
+        }
+        for (Relatorio r : resultado) {
+            double media = r.getValorTotal() / totalDias;
+            r.setMediaVendasDiarias(media);
+        }
+        return resultado;
     }
 
 }
