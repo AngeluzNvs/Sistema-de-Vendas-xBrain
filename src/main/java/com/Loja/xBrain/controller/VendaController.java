d024dc0085a0c51cf1836df19207d3bf2a1ededd
Angeluz <angeluznazer1@gmail.com>
2026-06-15 19:11:10 -0300

    coloquei @getMapping(/relatorio) para definir que as requisições do get devem vir para api - vendas - relatorio. @RequestParam serve pra pegar os parametros que sao digitados na url e o ultimo retorno serve para receber as datas e repassar para o Service

--- src/main/java/com/Loja/xBrain/controller/VendaController.java
+++ src/main/java/com/Loja/xBrain/controller/VendaController.java
@@ -1,13 +1,25 @@
 package com.Loja.xBrain.controller;
 
+import com.Loja.xBrain.dto.Relatorio;
+import com.Loja.xBrain.model.VendaModel;
 import com.Loja.xBrain.service.VendaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/vendas")
 public class VendaController {
     @Autowired
     private VendaService vendaService;
+    @PostMapping
+    public VendaModel criarVenda(@RequestBody VendaModel venda){
+        return vendaService.salvarVenda(venda);
+    }
+    @GetMapping("/relatorio")
+    public List<Relatorio> obterRelatorio(@RequestParam("/inicio") LocalDate inicio, @RequestParam("fim") LocalDate fim){
+        return vendaService.gerarRelatorio(inicio, fim);
+    }
 }
